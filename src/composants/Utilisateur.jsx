43532e970f5078502564223ca4d59fe02915f464
Andy Ke <andyke00@gmail.com>
2021-04-14 20:52:47 -0400

    Point D terminé

--- src/composants/Utilisateur.jsx
+++ src/composants/Utilisateur.jsx
@@ -7,8 +7,8 @@ export default function Utilisateur({utilisateur}) {
   return (
     <div className="Utilisateur">
       {/* Ex3 - Point D)iii) */}
-      <span className="nom">Alibaba</span>
-      <Avatar className="avatar" alt="Alibaba" src="" />
+      <span className="nom">{utilisateur.displayName}</span>
+      <Avatar className="avatar" alt={utilisateur.displayName} src={utilisateur.photoURL} />
       {/* Ex3 - Point C */}
       <Button 
         variant="outlined"
